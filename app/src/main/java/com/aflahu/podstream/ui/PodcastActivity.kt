d7edfb35a889072d83811ce988581c9cfcc36e18
abdullah <erka.j.p@gmail.com>
2020-07-09 16:35:50 +0700

    Initial commit

---
+++ app/src/main/java/com/aflahu/podstream/ui/PodcastActivity.kt
@@ -0,0 +1,12 @@
+package com.aflahu.podstream.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aflahu.podstream.R
+
+class PodcastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_podcast)
+    }
+}
