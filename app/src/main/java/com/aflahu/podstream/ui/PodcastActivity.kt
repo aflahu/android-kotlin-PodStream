c0d7ed3a48634ccb0149f19b5b796ed2307fca80
abdullah <erka.j.p@gmail.com>
2020-07-10 18:53:10 +0700

    add android widget SearchView

--- app/src/main/java/com/aflahu/podstream/ui/PodcastActivity.kt
+++ app/src/main/java/com/aflahu/podstream/ui/PodcastActivity.kt
@@ -1,8 +1,12 @@
 package com.aflahu.podstream.ui
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import com.aflahu.podstream.R
 import com.aflahu.podstream.repository.ItunesRepo
 import com.aflahu.podstream.service.ItunesService
@@ -22,5 +26,19 @@ class PodcastActivity : AppCompatActivity() {
         itunesRepo.searchByTerm("Android Developer") {
             Log.i(TAG, "Results = $it")
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
     }
 }
