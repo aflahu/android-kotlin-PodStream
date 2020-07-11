3e3471684d8b8718e62e6045281a875da064cacd
abdullah <erka.j.p@gmail.com>
2020-07-11 21:29:44 +0700

    add episode & podcast model

---
+++ app/src/main/java/com/aflahu/podstream/model/Podcast.kt
@@ -0,0 +1,12 @@
+package com.aflahu.podstream.model
+
+import java.util.*
+
+data class Podcast(
+    var feedUrl: String = "",
+    var feedTitle: String = "",
+    var feedDesc: String = "",
+    var imageUrl: String = "",
+    var lastUpdated: Date = Date(),
+    var episode: List<Episode> = listOf()
+)
