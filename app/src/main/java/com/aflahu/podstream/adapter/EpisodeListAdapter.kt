c83e654016829bbcae2312de93f3a736208bec15
abdullah <erka.j.p@gmail.com>
2020-07-13 17:23:47 +0700

    episode list adapter

---
+++ app/src/main/java/com/aflahu/podstream/adapter/EpisodeListAdapter.kt
@@ -0,0 +1,42 @@
+package com.aflahu.podstream.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.aflahu.podstream.R
+import com.aflahu.podstream.viewmodel.PodcastViewModel
+import kotlinx.android.synthetic.main.episode_item.view.*
+
+class EpisodeListAdapter(private var episodeViewList: List<PodcastViewModel.EpisodeViewData>?) :
+    RecyclerView.Adapter<EpisodeListAdapter.ViewHolder>() {
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        var episodeViewData: PodcastViewModel.EpisodeViewData? = null
+        val titleTextView: TextView = v.titleView
+        val descTextView: TextView = v.descView
+        val durationTextView: TextView = v.durationView
+        val releaseDateTextView: TextView = v.releaseDateView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.episode_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val episodeViewList = episodeViewList ?: return
+        val episodeView = episodeViewList[position]
+
+        holder.episodeViewData = episodeView
+        holder.titleTextView.text = episodeView.title
+        holder.descTextView.text = episodeView.description
+        holder.durationTextView.text = episodeView.duration
+        holder.releaseDateTextView.text = episodeView.releaseDate.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return episodeViewList?.size ?: 0
+    }
+}
