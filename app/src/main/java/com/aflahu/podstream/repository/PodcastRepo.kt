8e0a39f11713761cd5165988ca469eac62c0297c
abdullah <erka.j.p@gmail.com>
2020-07-13 16:37:42 +0700

    updating PodcastRepo.kt

--- app/src/main/java/com/aflahu/podstream/repository/PodcastRepo.kt
+++ app/src/main/java/com/aflahu/podstream/repository/PodcastRepo.kt
@@ -1,17 +1,61 @@
 package com.aflahu.podstream.repository
 
+import com.aflahu.podstream.model.Episode
 import com.aflahu.podstream.model.Podcast
+import com.aflahu.podstream.service.FeedService
+import com.aflahu.podstream.service.RssFeedResponse
 import com.aflahu.podstream.service.RssFeedService
-import javax.security.auth.callback.Callback
+import com.aflahu.podstream.util.DateUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-class PodcastRepo {
+class PodcastRepo(private var feedService: FeedService) {
     fun getPodcast(feedUrl: String, callback: (Podcast?) -> Unit) {
         val rssFeedService = RssFeedService()
 
-        rssFeedService.getFeed(feedUrl) {}
+        rssFeedService.getFeed(feedUrl) { feedResponse ->
+            var podcast: Podcast? = null
+            if (feedResponse != null) {
+                podcast = rssResponseToPodcast(feedUrl, "", feedResponse)
+            }
+            GlobalScope.launch(Dispatchers.Main) {
+                callback(podcast)
+            }
+        }
+    }
 
-        callback(
-            Podcast(feedUrl, "No Name", "No description", "No image")
+    private fun rssResponseToPodcast(
+        feedUrl: String,
+        imageUrl: String,
+        rssResponse: RssFeedResponse
+    ): Podcast? {
+        val items = rssResponse.episodes ?: return null
+
+        val description = if (rssResponse.description == "")
+            rssResponse.summary else rssResponse.description
+
+        return Podcast(
+            feedUrl,
+            rssResponse.title,
+            description,
+            imageUrl,
+            rssResponse.lastUpdated,
+            episodes = rssItemsToEpisodes(items)
         )
+    }
+
+    private fun rssItemsToEpisodes(episodeResponses: List<RssFeedResponse.EpisodeResponse>): List<Episode> {
+        return episodeResponses.map {
+            Episode(
+                it.guid ?: "",
+                it.title ?: "",
+                it.description ?: "",
+                it.url ?: "",
+                it.type ?: "",
+                DateUtils.xmlDateToDate(it.pubDate),
+                it.duration ?: ""
+            )
+        }
     }
 }
