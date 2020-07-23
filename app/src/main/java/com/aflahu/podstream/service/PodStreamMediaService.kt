6308ebfdc78f2e1a9fbff176b78cb835d3880a5e
abdullah <erka.j.p@gmail.com>
2020-07-24 03:45:29 +0700

    Create a MediaSession

--- app/src/main/java/com/aflahu/podstream/service/PodStreamMediaService.kt
+++ app/src/main/java/com/aflahu/podstream/service/PodStreamMediaService.kt
@@ -2,12 +2,26 @@ package com.aflahu.podstream.service
 
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
 
 class PodStreamMediaService : MediaBrowserServiceCompat() {
 
+    private lateinit var mediaSession: MediaSessionCompat
+
     override fun onCreate() {
         super.onCreate()
+        createMediaSession()
+    }
+
+    private fun createMediaSession() {
+        mediaSession = MediaSessionCompat.fromMediaSession(this, "PodstreamMediaService")
+
+        setSessionToken(mediaSession.sessionToken)
+
+        val callback = PodstreamMediaCallback(this, mediaSession)
+        mediaSession.setCallback(callback)
+
     }
 
     override fun onLoadChildren(
